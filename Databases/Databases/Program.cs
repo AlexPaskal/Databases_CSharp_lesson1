b7f0a8897eb1877d78b0bfb744b8353e8e9d0eea
Alex Paskal <alexpaskal@i.ua>
2016-08-02 13:47:31 +0300

    Step3 - static void Main() {}

--- Databases/Databases/Program.cs
+++ Databases/Databases/Program.cs
@@ -13,15 +13,22 @@ namespace Databases
         {
             try
             {
-                
+                Console.ForegroundColor = ConsoleColor.White;
+                Write_To_Database();
+                Console.ReadLine();
+                Read_From_Database();
             }
             catch (Exception ex)
             {
-                
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR!");
+                Console.WriteLine("Source: {0};", ex.Source);
+                Console.WriteLine("Message: {0};", ex.Message);
+                Console.WriteLine("HelpLink: {0}.", ex.HelpLink);
             }
             finally
             {
-                
+                Console.ReadLine();
             }
         }
         static void Write_To_Database()
